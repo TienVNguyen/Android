faa079e96a562e928c2f6fb37c734160c4a52c79
TienVNguyen <tien.workinfo@gmail.com>
2015-12-07 12:59:28 +0700

    Android Updated (12/07/2015)
    Init commit for Sunshine project which were generated base on Udacity's
    course (Developing Android Apps)

---
+++ Sunshine/app/src/main/java/com/training/tiennguyen/sunshine/MainActivityFragment.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2015. Self Training Systems, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by TienNguyen <tien.workinfo@gmail.com - tien.workinfo@icloud.com>, October 2015
+ */
+
+package com.training.tiennguyen.sunshine;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
