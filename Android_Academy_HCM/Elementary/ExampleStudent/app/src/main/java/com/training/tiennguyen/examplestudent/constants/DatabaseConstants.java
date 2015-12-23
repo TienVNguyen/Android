b358f32fbcc6983eb0ef0d78308dacf7025f686c
TienVNguyen <tien.workinfo@gmail.com>
2015-12-24 02:27:30 +0700

    Android Updated (12/24/2015)
    - Update ExampleStudent (Project): Add the missing 2 constants files.

---
+++ Android_Academy_HCM/Elementary/ExampleStudent/app/src/main/java/com/training/tiennguyen/examplestudent/constants/DatabaseConstants.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2015. Self Training Systems, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by TienNguyen <tien.workinfo@gmail.com - tien.workinfo@icloud.com>, October 2015
+ */
+
+package com.training.tiennguyen.examplestudent.constants;
+
+/**
+ * DatabaseConstants
+ *
+ * @author TienNguyen
+ */
+public class DatabaseConstants {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "GOOGLE";
+    public static final String TABLE_STUDENTS = "STUDENTS";
+    public static final String KEY_EMAIL = "EMAIL";
+    public static final String KEY_NAME = "NAME";
+    public static final String KEY_GENDER = "GENDER";
+    public static final String KEY_PHONE = "PHONE";
+    public static final String KEY_MAJOR = "MAJOR";
+    public static final String KEY_AVATAR = "AVATAR";
+}
