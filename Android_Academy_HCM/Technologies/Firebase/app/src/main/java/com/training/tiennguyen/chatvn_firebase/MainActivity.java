fe4dc96034fff52bdbd04517d6fd04e899e1fea6
TienVNguyen <tien.workinfo@gmail.com>
2016-03-06 22:33:15 +0700

    Android Updated (03/06/2016)
    - Add folder \Android\Android_Academy_HCM\Technologies\
    + Add Firebase (Project)

---
+++ Android_Academy_HCM/Technologies/Firebase/app/src/main/java/com/training/tiennguyen/chatvn_firebase/MainActivity.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2016. Self Training Systems, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by TienNguyen <tien.workinfo@gmail.com - tien.workinfo@icloud.com>, October 2015
+ */
+
+package com.training.tiennguyen.chatvn_firebase;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.client.Firebase;
+import com.firebase.ui.FirebaseListAdapter;
+import com.firebase.ui.auth.core.AuthProviderType;
+import com.firebase.ui.auth.core.FirebaseLoginBaseActivity;
+import com.firebase.ui.auth.core.FirebaseLoginError;
+import com.training.tiennguyen.chatvn_firebase.model.ChatMessage;
+
+public class MainActivity extends FirebaseLoginBaseActivity {
+    private Firebase mFirebaseRef;
+    FirebaseListAdapter<ChatMessage> mListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initial connection between our code and its Firebase backend
+        Firebase.setAndroidContext(this);
+
+        setContentView(R.layout.activity_main);
+
+        // Connect to the specific database
+        mFirebaseRef = new Firebase("https://chatvn-firebase.firebaseio.com/");
+
+        final EditText textEdit = (EditText) this.findViewById(R.id.text_edit);
+
+        // Event when sendButton is click
+        Button sendButton = (Button) this.findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = textEdit.getText().toString();
+                ChatMessage message = new ChatMessage("Android User", text);
+                mFirebaseRef.push().setValue(message);
+                textEdit.setText("");
+            }
+        });
+
+        // If data contains records, list will appear
+        final ListView listView = (ListView) this.findViewById(android.R.id.list);
+        mListAdapter = new FirebaseListAdapter<ChatMessage>(this, ChatMessage.class,
+                android.R.layout.two_line_list_item, mFirebaseRef) {
+            @Override
+            protected void populateView(View v, ChatMessage model, int position) {
+                ((TextView)v.findViewById(android.R.id.text1)).setText(model.getName());
+                ((TextView)v.findViewById(android.R.id.text2)).setText(model.getText());
+            }
+        };
+        listView.setAdapter(mListAdapter);
+
+        // Login Authentication
+        Button loginButton = (Button) this.findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFirebaseLoginPrompt();
+            }
+        });
+    }
+
+    @Override
+    protected Firebase getFirebaseRef() {
+        return null;
+    }
+
+    @Override
+    protected void onFirebaseLoginProviderError(FirebaseLoginError firebaseLoginError) {
+
+    }
+
+    @Override
+    protected void onFirebaseLoginUserError(FirebaseLoginError firebaseLoginError) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setEnabledAuthProvider(AuthProviderType.PASSWORD);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mListAdapter.cleanup();
+    }
+}
---
+++ Android_Academy_HCM/Technologies/Firebase/app/src/main/java/com/training/tiennguyen/chatvn_firebase/model/ChatMessage.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016. Self Training Systems, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by TienNguyen <tien.workinfo@gmail.com - tien.workinfo@icloud.com>, October 2015
+ */
+
+package com.training.tiennguyen.chatvn_firebase.model;
+
+/**
+ * ChatMessage
+ *
+ * Created by TienNguyen on 06/03/2016.
+ */
+public class ChatMessage {
+    private String name;
+    private String text;
+
+    public ChatMessage() {
+        // Necessary for Firebase's deserializer
+    }
+
+    public ChatMessage(String name, String text) {
+        this.name = name;
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
