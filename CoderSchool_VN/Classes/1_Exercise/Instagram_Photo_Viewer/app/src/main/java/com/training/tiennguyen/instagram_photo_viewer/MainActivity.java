2ff3b254f3cdcdb1b6d38f3ddc9bdadbac1574f4
TienVNguyen <tien.workinfo@gmail.com>
2016-03-10 03:02:08 +0700

    Revert "Android Updated (03/10/2016)"
    - This reverts commit

---
+++ CoderSchool_VN/Classes/1_Exercise/Instagram_Photo_Viewer/app/src/main/java/com/training/tiennguyen/instagram_photo_viewer/MainActivity.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2016. Self Training Systems, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by TienNguyen <tien.workinfo@gmail.com - tien.workinfo@icloud.com>, October 2015
+ */
+
+package com.training.tiennguyen.instagram_photo_viewer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
