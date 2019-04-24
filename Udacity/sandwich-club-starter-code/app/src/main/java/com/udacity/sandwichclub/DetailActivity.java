aee00b5ccfada11aa889fbf3a85e5b306007fdf9
TienVNguyen <tien.workinfo@gmail.com>
2019-04-24 00:32:23 -0700

    Finish Core Functionality
    - Finish all core funcationality and ready to check.

--- Udacity/sandwich-club-starter-code/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ Udacity/sandwich-club-starter-code/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,23 +4,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private ImageView mIngredientsIv;
+    private TextView mOriginTv;
+    private TextView mDescriptionTv;
+    private TextView mIngredientsTv;
+    private TextView mAlsoKnownTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        mIngredientsIv = findViewById(R.id.image_iv);
+        mOriginTv = findViewById(R.id.origin_tv);
+        mDescriptionTv = findViewById(R.id.description_tv);
+        mIngredientsTv = findViewById(R.id.ingredients_tv);
+        mAlsoKnownTv = findViewById(R.id.also_known_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,10 +56,13 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
+
         Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
+                .load("https://upload.wikimedia.org/wikipedia/commons/4/48/Chivito1.jpg")
+                .placeholder(R.mipmap.ic_launcher)
+                .error(R.mipmap.ic_launcher_round)
+                .into(mIngredientsIv);
 
         setTitle(sandwich.getMainName());
     }
@@ -56,7 +72,13 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        mOriginTv.setText(sandwich.getPlaceOfOrigin());
+        mDescriptionTv.setText(sandwich.getDescription());
 
+        List<String> ingredients = sandwich.getIngredients();
+        if (!ingredients.isEmpty()) mIngredientsTv.setText(String.valueOf(ingredients));
+        List<String> alsoKnownAs = sandwich.getAlsoKnownAs();
+        if (!alsoKnownAs.isEmpty()) mAlsoKnownTv.setText(String.valueOf(alsoKnownAs));
     }
 }
